9b869cdfc0180f1c18d9f9fc8d268ee3b372cfd0
jennykgb <jennykgb@gmail.com>
2023-10-04 17:47:52 -0400

    Complete Memes generator

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header.jsx'
+import Meme from './components/Meme.jsx'
+
+function App() {
+  
+
+  return (
+   <div>
+    <Header />
+    <Meme />
+   </div>
+  )
+}
+
+export default App
